be64c3f8e408c726d8338da1eda73322b83e789f
Moiz Qureshi <m.moiz782008@gmail.com>
2025-02-15 02:23:56 +0500

    update
    push

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,6 +37,15 @@ export default async function Navbar (){
             <div className="flex flex-col md:flex-row justify-around bg-[#272343] text-sm text-[#FFFFFF] py-2 lg:gap-40">
                 <p className="flex justify-center md:justify-start"><TiTick className="text-2xl mr-1"/>{CMSHeader.paragraph}</p>
                 <div className="flex justify-center md:justify-start gap-4 ">
+                    <select className="bg-indigo-950">
+                        <option>Eng</option>
+                        <option>....</option>
+                        <option>....</option>
+                        <option>....</option>
+                        <option>Other</option>
+                    </select>
+                    <Link href="page" className="hover:underline">Faqs</Link>
+                    <Link href="shop"className="flex hover:underline"><BiErrorCircle className="mt-1 mr-1"/>Need Help</Link>
                     <div className="hover:underline">
                       <ClerkProvider>
                         <SignedOut>
@@ -47,21 +56,22 @@ export default async function Navbar (){
                         </SignedIn>
                       </ClerkProvider>
                     </div>
-                    <select className="bg-indigo-950">
-                        <option>Eng</option>
-                        <option>....</option>
-                        <option>....</option>
-                        <option>....</option>
-                        <option>Other</option>
-                    </select>
-                    <Link href="page" className="hover:underline">Faqs</Link>
-                    <Link href="shop"className="flex hover:underline"><BiErrorCircle className="mt-1 mr-1"/>Need Help</Link>
                 </div>
             </div>
             <div className="flex justify-around bg-[#F0F2F3]">
                 <h2 className="flex md:mr-48 text-[#272343] text-3xl font-medium my-3"><GiSofa className="text-[#029FAE] text-5xl mr-1"/><span className="mt-2">{CMSHeader.heading}</span></h2>
                 <div className="flex gap-4">
                 <Link href="cart" className="flex md:ml-40 bg-[#FFFFFF] rounded-xl text-[#272343] font-medium py-3 px-5 my-3 transition-transform transform hover:scale-105"><LuShoppingCart className="mr-2 text-xl"/>Cart</Link>
+                  <div className="bg-[#FFFFFF] rounded-xl text-[#272343] font-medium py-3 px-5 my-3 transition-transform transform hover:scale-105">
+                    <ClerkProvider>
+                      <SignedOut>
+                        <SignInButton />
+                      </SignedOut>
+                      <SignedIn>
+                        <UserButton />
+                      </SignedIn>
+                    </ClerkProvider>
+                  </div>
                 </div>
             </div>
             <div className="flex justify-around text-[#636270] font-semibold lg:gap-16 sm:mt-6 bg-[#FFFFFF]">
@@ -72,16 +82,6 @@ export default async function Navbar (){
                     <li className="hover:text-[#029FAE] transition-transform transform hover:scale-105"><Link href="products">Products</Link></li>
                     <li className="hover:text-[#029FAE] transition-transform transform hover:scale-105"><Link href="page">Pages</Link></li>
                     <li className="hover:text-[#029FAE] transition-transform transform hover:scale-105"><Link href="about">About</Link></li>
-                    <li className="hover:text-[#029FAE] transition-transform transform hover:scale-105">
-                        <ClerkProvider>
-                              <SignedOut>
-                                <SignInButton />
-                              </SignedOut>
-                              <SignedIn>
-                                <UserButton />
-                              </SignedIn>
-                        </ClerkProvider>
-                    </li>
                 </ul>
                 </div>
                 <p className="hidden md:block">Contact: <span className="text-[#272343]">{CMSHeader.phone}</span></p>
